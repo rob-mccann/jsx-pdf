e8a1d9f88e4354567c9703b38ac8a60162e601a4
Rob McCann <rob.mccann@schibsted.com>
2017-04-06 16:25:40 +0100

    Add mocha/chai

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+
+describe('#jsx-pdf', () => {
+   describe('hello world', () => {
+       it('should return true, when true is true', () => {
+           expect(true).to.be.true;
+       })
+   })
+});
