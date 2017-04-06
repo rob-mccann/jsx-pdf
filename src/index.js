d46aef99568660f5b228b7bde70a2d98d9704a4e
Rob McCann <rob.mccann@schibsted.com>
2017-04-06 17:01:09 +0100

    Removes react and updates traversal library to work with babel jsx transpiler (#1)

--- src/index.js
+++ src/index.js
@@ -1,180 +1,28 @@
 'use strict';
 
 // libs
-import fs from 'fs';
-import path from 'path';
-import emptyObject from 'fbjs/lib/emptyObject';
 import omit from 'lodash/omit';
 import pick from 'lodash/pick';
 import compact from 'lodash/compact';
-
-// react
-import ReactFiberReconciler from 'react-dom/lib/ReactFiberReconciler';
-import ReactGenericBatching from 'react-dom/lib/ReactGenericBatching';
 
 // pdf
 import PDFMake from 'pdfmake';
 import OpenSans from './fonts';
 
-const createContainer = (element) => {
-    const doc = {
-        content: [],
-        defaultStyle: {
-            font: 'OpenSans',
-            fontSize: 10
-        }
-    };
 
-    const container = {
-        children: [],
-        tag: 'CONTAINER',
-        doc,
-        firstPageSkipped: false,
-    };
+function toPDFMake(tag, doc) {
+    if (typeof tag === 'string') { // text element
+        return tag;
+    }
 
-    const root = PDFRenderer.createContainer(container);
-    PDFRenderer.updateContainer(element, root, null, null);
-
-    return container;
-};
-
-
-const PDFRenderer = ReactFiberReconciler({
-    getRootHostContext() {
-        return emptyObject;
-    },
-
-    getChildHostContext() {
-        return emptyObject;
-    },
-
-    prepareForCommit() {
-        // noop
-    },
-
-    resetAfterCommit() {
-        // noop
-    },
-
-    createInstance(
-        type,
-        props,
-        rootContainerInstance,
-    ) {
-        return {
-            type,
-            props,
-            children: [],
-            rootContainerInstance,
-            tag: 'INSTANCE',
-        };
-    },
-
-    appendInitialChild(parentInstance, child) {
-        const index = parentInstance.children.indexOf(child);
-        if (index !== -1) {
-            parentInstance.children.splice(index, 1);
-        }
-        parentInstance.children.push(child);
-    },
-
-    finalizeInitialChildren() {
-        return false;
-    },
-
-    prepareUpdate() {
-        return true;
-    },
-
-    commitUpdate(
-        instance,
-        type,
-        oldProps,
-        newProps,
-        rootContainerInstance,
-        internalInstanceHandle,
-    ) {
-        // noop
-    },
-
-    commitMount(
-        instance,
-        type,
-        newProps,
-        rootContainerInstance,
-        internalInstanceHandle,
-    ) {
-        // noop
-    },
-
-    shouldSetTextContent() {
-        return false;
-    },
-
-    resetTextContent(testElement) {
-        // noop
-    },
-
-    createTextInstance(
-        text,
-    ) {
-        return {
-            text,
-            tag: 'TEXT',
-        };
-    },
-
-    commitTextUpdate(textInstance, oldText, newText) {
-        textInstance.text = newText;
-    },
-
-    appendChild(parentInstance, child) {
-        const index = parentInstance.children.indexOf(child);
-        if (index !== -1) {
-            parentInstance.children.splice(index, 1);
-        }
-        parentInstance.children.push(child);
-    },
-
-    insertBefore(parentInstance, child, beforeChild) {
-        const index = parentInstance.children.indexOf(child);
-        if (index !== -1) {
-            parentInstance.children.splice(index, 1);
-        }
-        const beforeIndex = parentInstance.children.indexOf(beforeChild);
-        parentInstance.children.splice(beforeIndex, 0, child);
-    },
-
-    removeChild(parentInstance, child) {
-        const index = parentInstance.children.indexOf(child);
-        parentInstance.children.splice(index, 1);
-    },
-
-    scheduleAnimationCallback(fn) {
-        setTimeout(fn);
-    },
-
-    scheduleDeferredCallback(fn) {
-        setTimeout(fn, 0, { timeRemaining: Infinity });
-    },
-
-    useSyncScheduling: true,
-
-    getPublicInstance(inst) {
-        return inst;
-    },
-});
-
-function traverseComponentTree(inst) {
-    const { doc } = inst.rootContainerInstance;
-    const { children, ...props } = inst.props;
+    const { children, elementName, ...attributes } = tag;
 
     let resolvedChildren = undefined;
 
     // go down the tree and resolve children elements. This is done up here as we choose to do different
     // things with children, based on the tag
-    if (inst.children && inst.children.length) {
-        resolvedChildren = compact(inst.children.map(toPDF)); // compact removes empty elements
+    if (children && children.length) {
+        resolvedChildren = compact(children.map((child) => toPDFMake(child, doc))); // compact removes empty elements
     }
 
     /**
@@ -182,17 +30,17 @@ function traverseComponentTree(inst) {
      *
      * Converts the React-like syntax to something PDFMake understands.
      */
-    switch (inst.type) {
+    switch (elementName) {
         case 'document':
-            if (props.size) {
-                doc.pageSize = props.size;
+            if (attributes.size) {
+                doc.pageSize = attributes.size;
             }
 
-            if (props.margin) {
-                doc.pageMargins = props.margin;
+            if (attributes.margin) {
+                doc.pageMargins = attributes.margin;
             }
 
-            doc.info = pick(props, ['title', 'author', 'subject', 'keywords']);
+            doc.info = pick(attributes, ['title', 'author', 'subject', 'keywords']);
 
             if (inst.children && inst.children.length) {
                 doc.content = doc.content.concat(resolvedChildren || []);
@@ -202,68 +50,51 @@ function traverseComponentTree(inst) {
             break;
         case 'footer':
         case 'header':
-            doc[inst.type] = [ { stack: [ ...resolvedChildren ], ...props } ];
+            doc[elementName] = [ { stack: [ ...resolvedChildren ], ...attributes } ];
             return false;
             break;
         case 'stack':
         case 'group':
-            return { stack: resolvedChildren || children, ...props }; // children is only for TEXT tags
+            return { stack: resolvedChildren || children, ...attributes }; // children is only for TEXT tags
             break;
         case 'text':
-            return { text: resolvedChildren || children, ...props }; // children is only for TEXT tags
+            return { text: resolvedChildren || children, ...attributes }; // children is only for TEXT tags
             break;
         case 'columns':
-            return { columns: resolvedChildren || [], ...props };
+            return { columns: resolvedChildren || [], ...attributes };
             break;
         case 'image':
-            return { image: props.src, ...(omit(props, 'src')) };
+            return { image: attributes.src, ...(omit(attributes, 'src')) };
         case 'table':
-            return { table: { body: resolvedChildren, ...(pick(props, ['headerRows', 'widths'])) }, ...props };
+            return { table: { body: resolvedChildren, ...(pick(attributes, ['headerRows', 'widths'])) }, ...attributes };
             break;
         case 'row':
             return [ ...resolvedChildren ];
         case 'cell':
-            return { stack: resolvedChildren, ...props };
+            return { stack: resolvedChildren, ...attributes };
             break;
         default:
             break;
     }
-
-
 }
 
-// is the tag a component or text object?
-function toPDF(inst) {
-    switch (inst.tag) {
-        case 'TEXT':
-            // if the component contains text only
-            return inst.text;
-        case 'INSTANCE':
-            // if the component contains an instance of a component
-            return traverseComponentTree(inst);
-        default:
-            throw new Error('Unexpected node type in toPDF: ' + inst.tag);
-    }
-}
+export default function render(elementJSON) {
+    const doc = {
+        content: [],
+        defaultStyle: {
+            font: 'OpenSans',
+            fontSize: 10
+        }
+    };
 
-const ReactPDFFiberRenderer = {
-    render(element) {
-        // Create the container and add the PDF template to the container
-        const container = createContainer(element);
+    // Recursively traverse the PDF template, converting the React-like syntax to pdfmake's syntax
+    const doc2 = toPDFMake(elementJSON, doc);
 
-        // Recursively traverse the PDF template, converting the React-like syntax to pdfmake's syntax
-        const doc2 = toPDF(container.children[0]);
+    // the argument here contains a list of fonts on the filesystem. see fonts.js
+    const pdf = (new PDFMake({
+        OpenSans
+    })).createPdfKitDocument(doc2);
 
-        // the argument here contains a list of fonts on the filesystem. see fonts.js
-        const pdf = (new PDFMake({
-            OpenSans
-        })).createPdfKitDocument(doc2);
-
-        // return the stream for the caller to handle (usually by pushing straight to s3 or to the filesystem
-        return pdf;
-    },
-
-    unstable_batchedUpdates: ReactGenericBatching.batchedUpdates,
+    // return the stream for the caller to handle (usually by pushing straight to s3 or to the filesystem
+    return pdf;
 };
-
-export default ReactPDFFiberRenderer;
