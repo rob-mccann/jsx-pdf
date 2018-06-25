2a9dc01adebf90d76b61413ea54740effba61f80
Yaroslav Borcheninov <yaroslav.borcheninov@schibsted.com>
2018-06-25 16:57:51 +0100

    [SA-4992] Remove OpenSans light

--- src/fonts.js
+++ src/fonts.js
@@ -1,7 +1,6 @@
 import path from 'path';
 
 const OpenSans = {
-  light: path.resolve(__dirname, '../fonts/OpenSans/OpenSans-Light.ttf'),
   normal: path.resolve(__dirname, '../fonts/OpenSans/OpenSans-Regular.ttf'),
   bold: path.resolve(__dirname, '../fonts/OpenSans/OpenSans-Bold.ttf'),
   italics: path.resolve(__dirname, '../fonts/OpenSans/OpenSans-Italic.ttf'),
